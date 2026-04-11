c9d0f5d6f0ff100063922fbe2b1caa6307e5e84a
Giuseppe <giuseppe.troina@tiscali.it>
2026-04-11 21:16:00 +0200

    first qiskit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Deployment-friendly entrypoint for the debate backend."""
+
+#####  conda activate DOUBT
+#####  uvicorn main:app --reload
+#####  http://127.0.0.1:8000
+
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
+
+sampler = StatevectorSampler()
+result = sampler.run([qc], shots=1024).result()
+print(result[0].data.meas.get_counts())
