5a04eafb9e7fdf586b66b256ef1d46b4319bed8e
Giuseppe <giuseppe.troina@tiscali.it>
2026-04-12 10:32:47 +0200

    first py

--- main.py
+++ main.py
@@ -15,3 +15,11 @@ qc.measure_all()
 sampler = StatevectorSampler()
 result = sampler.run([qc], shots=1024).result()
 print(result[0].data.meas.get_counts())
+
+import matplotlib.pyplot as plt
+from qiskit.visualization import plot_histogram
+
+counts = result[0].data.meas.get_counts()
+plot_histogram(counts)
+
+plt.show()
