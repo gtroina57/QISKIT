054d1ce4981d878ae9fec3a1abd8d1966a65c38b
Giuseppe <giuseppe.troina@tiscali.it>
2026-09-13 22:58:25 +0200

    credentials

--- Credentials.py
+++ Credentials.py
@@ -1,8 +1,10 @@
+import getpass
+
 from qiskit_ibm_runtime import QiskitRuntimeService
 
 QiskitRuntimeService.save_account(
-token=getpass.getpass("IBM Quantum token: "),,
-instance="open-instance",
-channel="ibm_quantum_platform",
-overwrite=True,
+    token=getpass.getpass("IBM Quantum token: "),
+    instance="open-instance",
+    channel="ibm_quantum_platform",
+    overwrite=True,
 )
